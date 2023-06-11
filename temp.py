a9bdbe26f4199af3bfedd269e386f643192b0e8d
eefeturkan <118450477+eefeturkan@users.noreply.github.com>
2023-06-11 22:56:16 +0300

    Add files via upload

--- temp.py
+++ temp.py
@@ -1,7 +1,14 @@
+import pandas as pd
+import sqlite3
 
-#sözlük
 kullanicilar = {}
 
+df = pd.read_excel("kullanici_bilgileri.xlsx")
+
+kullanici_adlari = df["Kullanıcı Adı"]
+sifreler = df["Şifre"]
+
+kullanicilar = dict(zip(kullanici_adlari, sifreler))
 
 def kullanici_menu():
     while True:
@@ -19,40 +26,133 @@ def kullanici_menu():
             break
         else:
             print("Geçersiz seçim. Lütfen 1-3 arasında bir sayı girin.")
+            
 
 def deprem_kontrolu_baslat():
     print("Deprem güvenliği kontrolü başlatılıyor")
-    # Kontrol işlemleri burada olacak
-    # Kontrol işlemleri burada olacak
-    # Kontrol işlemleri burada olacak
-    # Kontrol işlemleri burada olacak
+    il= input("Bulundugunuz ili giriniz ")
+    adres = input("Lütfen bina adresini giriniz. ")
+    yas = int(input("Binanın yaşı kaç ? "))
+    kat_sayisi=int(input("Binanın kat sayisini giriniz "))
+    malzeme = input("Binada hangi malzemeler kullanıldı? (beton, çelik, ahşap) ")
+    
+    deprem_puani = 0
+    
+    if yas <= 10:
+        deprem_puani += 5
+    elif yas <= 20:
+        deprem_puani += 7
+    else:
+        deprem_puani += 10
+    
+ 
+    if kat_sayisi <=3:
+        deprem_puani += 5 
+    elif kat_sayisi <=7:
+        deprem_puani += 7
+    else:
+        deprem_puani += 10
+    
+  
+    if malzeme == "beton":
+        deprem_puani += 5
+    elif malzeme == "çelik":
+        deprem_puani += 8
+    elif malzeme == "ahşap":
+        deprem_puani += 10
+    
+    birinci_derece_deprem_illeri = ["İzmir", "Balıkesir", "Manisa", "Muğla", "Aydın", "Denizli", "Isparta", "Uşak", "Bursa", "Bilecik", "Yalova", "Sakarya", "Düzce", "Kocaeli", "Kırşehir", "Bolu", "Karabük", "Hatay", "Bartın", "Çankırı", "Tokat", "Amasya", "Çanakkale", "Erzincan", "Tunceli", "Bingöl", "Muş", "Hakkari", "Osmaniye", "Kırıkkale", "Siirt"]
+    
+    ikinci_derece_deprem_illeri = ["Tekirdağ", "İstanbul", "Bitlis", "Kahramanmaraş", "Van", "Adıyaman", "Şırnak", "Zonguldak", "Tekirdağ", "Afyon", "Samsun", "Antalya", "Erzurum", "Kars", "Ardahan", "Batman", "Iğdır", "Elazığ", "Diyarbakır", "Adana", "Eskişehir", "Malatya", "Kütahya", "Çankırı", "Uşak", "Ağrı", "Çorum"]
+    
+    ucuncu_derece_deprem_illeri = ["Eskişehir", "Antalya", "Tekirdağ", "Edirne", "Sinop", "İstanbul", "Kastamonu", "Ordu", "Samsun", "Giresun", "Artvin", "Şanlıurfa", "Mardin", "Kilis", "Adana", "Gaziantep", "Kahramanmaraş", "Sivas", "Gümüşhane", "Bayburt", "Kayseri", "Yozgat", "Çorum", "Ankara", "Konya", "Mersin", "Nevşehir"]
+    
+    dorduncu_besinci_derece_deprem_illeri = ["Sinop", "Giresun", "Trabzon", "Rize", "Artvin", "Kırklareli", "Ankara", "Edirne", "Adana", "Nevşehir", "Niğde", "Aksaray", "Konya", "Karaman"]
+    
+    if il in birinci_derece_deprem_illeri:
+        deprem_puani += 10
+    elif il in ikinci_derece_deprem_illeri:
+        deprem_puani += 8
+    elif il in ucuncu_derece_deprem_illeri:
+        deprem_puani += 6
+    elif il in dorduncu_besinci_derece_deprem_illeri:
+        deprem_puani += 4
+    else:
+        print("Geçersiz il girdiniz.")
+        return
+    
+ 
+    print("Deprem puanı:", deprem_puani)
     print("Kontrol tamamlandı.")
-
+    
+    if 0 <= deprem_puani <= 19:
+        risk_durumu = "Az riskli"
+    elif 20 <= deprem_puani <= 35:
+        risk_durumu = "Orta riskli"
+    elif deprem_puani > 35:
+        risk_durumu = "Yüksek riskli"
+    
+    print("Risk durumu:", risk_durumu)
+    
+    conn = sqlite3.connect('deprem_veritabani.db')
+    cursor = conn.cursor()
+     
+    cursor.execute('''CREATE TABLE IF NOT EXISTS binalar
+                       (adres TEXT, yas INTEGER, malzeme TEXT, deprem_puani INTEGER, risk_durumu TEXT)''')
+     
+    cursor.execute("INSERT INTO binalar VALUES (?, ?, ?, ?, ?, ?)", (il, adres, yas, malzeme, deprem_puani, risk_durumu))
+     
+    conn.commit()
+    conn.close()
+    
 def raporlari_goruntule():
     print("Deprem güvenliği kontrol raporları görüntüleniyor...")
-    # Raporları görüntüleme işlemleri burada olacak
+    conn = sqlite3.connect('deprem_veritabani.db')
+    cursor = conn.cursor()
+    
+    cursor.execute("SELECT * FROM binalar")
+    raporlar = cursor.fetchall()
+    
+    for rapor in raporlar:
+        il = rapor [0]
+        adres = rapor[1]
+        yas = rapor[2]
+        malzeme = rapor[3]
+        deprem_puani = rapor[4]
+        risk_durumu = rapor [5]
+        
+        print("İl:", il)
+        print("Adres:", adres)
+        print("Yaş:", yas)
+        print("Malzeme:", malzeme)
+        print("Deprem Puanı:", deprem_puani)
+        print("Risk Durumu:", risk_durumu)
+        print("------------------------")
 
-
-#kullanıcı ekleme fonksiyonu
 def kullanici_ekle():
+    df = pd.read_excel("kullanici_bilgileri.xlsx")
+    
     kullanici_adi = input("Kullanıcı adını girin: ")
     sifre = input("Şifrenizi girin: ")
     kullanicilar[kullanici_adi] = sifre  #kullanıcı adı ve şifreyi birbiriyle eşleştiriyor
     print("Kullanıcı başarıyla eklendi.")
+    
+    yeni_giris = pd.DataFrame({"Kullanıcı Adı": [kullanici_adi], "Şifre": [sifre]})
+    df = df.append(yeni_giris, ignore_index=True)
+    df.to_excel("kullanici_bilgileri.xlsx", index=False) 
 
-#kullanıcı girişi fonksiyonu
 def kullanici_giris():
+
     while True:
         kullanici_adi = input("Kullanıcı adınızı girin: ")
         sifre = input("Şifrenizi girin: ")
-        if kullanicilar.get(kullanici_adi) == sifre: #girilen kullanıcı adı ve şifrenin uyuşup uyuşmadığını kontrol ediyor
+        if kullanicilar.get(kullanici_adi) == sifre:  # Girilen kullanıcı adı ve şifrenin doğruluğunu kontrol ediyor
             print("Giriş başarılı.")
             kullanici_menu()
             break
         else:
             print("Kullanıcı adı veya şifre hatalı.")
 
-#şifre değiştirme fonksiyonu
 def sifre_yenile():
     kullanici_adi = input("Kullanıcı adınızı girin: ")
     if kullanici_adi in kullanicilar:
@@ -63,14 +163,12 @@ def sifre_yenile():
         print("Kullanıcı adı hatalı.")
 
 
-# Admin için yeni kullanıcı ekleme fonksiyonu
 def admin_kullanici_ekle():
     kullanici_adi = input("Kullanıcı adını girin: ")
     sifre = input("Şifrenizi girin: ")
     kullanicilar[kullanici_adi] = sifre
     print("Kullanıcı başarıyla eklendi.")
 
-# Kullanıcı silme fonksiyonu
 def kullanici_sil():
     kullanici_adi = input("Silmek istediğiniz kullanıcı adını girin: ")
     if kullanici_adi in kullanicilar:
@@ -81,7 +179,7 @@ def kullanici_sil():
 
 
 
-#admin girişi fonksiyonu
+
 def admin_giris():
     while True:
         kullanici_adi = input("Kullanıcı adınızı girin: ")
@@ -93,7 +191,7 @@ def admin_giris():
         else:
             print("Kullanıcı adı veya şifre hatalı. Lütfen tekrar deneyin.")
 
-# Admin menüsü
+
 def admin_menu():
     while True:
         print("Admin Menüsü")
@@ -103,7 +201,7 @@ def admin_menu():
         secim = input("Yapmak istediğiniz işlemi seçin (1-3): ")
 
         if secim == "1":
-            admin_kullanici_ekle()  # Yeni kullanıcı ekleme fonksiyonunu çağır
+            admin_kullanici_ekle() 
         elif secim == "2":
             kullanici_sil()
         elif secim == "3":
@@ -113,8 +211,6 @@ def admin_menu():
             print("Geçersiz seçim. Lütfen 1-3 arasında bir sayı girin.")
 
 
-
-#ana menü
 while True:
     print("Deprem Güvenliği Kontrol Uygulaması")
     print("1- Sisteme Üye Ol")
